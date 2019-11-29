efce4c2c208336c852f22182b70dbff07b41e4f1
tfpoivic <tfpoivic@gmail.com>
2019-11-30 01:43:19 +0800

    Origin

---
+++ Assets/Scripts/ChangeScene/Fade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    SceneTransition sceneTransition;
+    void Start() {
+        sceneTransition = GameObject.FindGameObjectWithTag("Player").GetComponent<SceneTransition>();
+    }
+    public void FadeEvent() {
+        sceneTransition.OnFadeComplete();
+    }
+}
